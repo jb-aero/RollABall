ca8b0088b1dcc52dd33c2bb2545bc0c757dd3260
jb-aero <jb_aero@zeoldcraft.com>
2017-02-13 16:51:18 -0500

    Added picking up, and scoring

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+	public Text scoreText;
+	public Text winText;
 
     private Rigidbody rb;
+	private int count;
 
 	void Start () {
         rb = GetComponent<Rigidbody>();
+		count = 0;
+		updateScore ();
+		winText.text = "";
 	}
 
     // Called for physics
@@ -22,4 +29,23 @@ public class PlayerController : MonoBehaviour {
 
         rb.AddForce(movement * speed);
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+			count++;
+			updateScore ();
+        }
+    }
+
+	void updateScore()
+	{
+		scoreText.text = "Score: " + count.ToString ();
+		if (count >= 12)
+		{
+			winText.text = "YOU WIN!!";
+		}
+	}
 }
